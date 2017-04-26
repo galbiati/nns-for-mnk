f5b4e6bdcb6872d1259bcb600358f47bd789ac72
gianni <galbiatig@gmail.com>
2017-04-26 12:06:38 -0500

    prepare for hpc runs

--- hpc_run.sh
+++ hpc_run.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-#PBS --nodes=1:ppn=4:gpus=1:titan
-#PBS --ntasks=1
-#PBS --time=48:00:00
-#PBS --mem=8GB
-#PBS --job-name=MNKnnets
-#PBS --mail-type=END
-#PBS --mail-user=gvg218@nyu.edu
-#PBS --output=MNKnnet_%j.out
+#PBS -l nodes=1:ppn=4:gpus=1:titan
+#PBS -l walltime=48:00:00
+#PBS -l mem=8GB
+#PBS -N MNKnnets
+#PBS -M gvg218@nyu.edu
 
 module purge
 module load python/intel/3.5.1
